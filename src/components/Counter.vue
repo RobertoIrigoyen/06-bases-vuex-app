4865b5a0db198844c7676ef743cd14cdb4201daa
ROBERTO IRIGOYEN L-BAISSON <ririlop@mdef.es>
2024-05-14 14:10:00 +0200

    Mutaciones

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,6 +3,11 @@
     <h1>Counter - Vuex</h1>
     <h2>Access direct: {{ $store.state.count }}</h2>
     <h2>Cumputes: {{ counComputed }}</h2>
+
+    <button @click="increment">Incrementar 1</button>
+    <button @click="incrementBy(5)">Incrementar 5</button>
+    <button>Incrementar Random</button>
+
     <h1>MapState</h1>
     <h2>mapState: {{ count }}</h2>
     <h2>mapState: {{ lastMutation }}</h2>
@@ -16,13 +21,21 @@ export default {
     counComputed() {
       return this.$store.state.count
     },
-    ...mapState(['count','lastMutation'])
+    ...mapState(['count', 'lastMutation'])
     //...mapState({
     //     count: state => state.count,
     //     // lastMutation: state => state.lastMutation
     //     lastMutation: 'lastMutation'
     // })
   },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    },
+    incrementBy(val) {
+      this.$store.commit('incrementBy', val)
+    }
+  }
 
 
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
 import { createStore } from "vuex";
 
-
 export default createStore({
-    state:{
-        count: 1,
-        lastMutation: 'holiwi'
+  state: {
+    count: 1,
+    lastMutation: "holiwi",
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+      state.lastMutation = "holitiwi";
+    },
+    incrementBy(state, val){
+        state.count += val
+        state.lastMutation = 'incrementado en un valor ' + val
     }
-})
+  },
+});
